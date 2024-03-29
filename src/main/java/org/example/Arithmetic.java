af22670b170794f686c69d2043a86c712d816a6a
mahdi <mmagrounmahdi@gmail.com>
2024-03-30 00:57:15 +0100

    passing the addition of 2 negative value

--- src/main/java/org/example/Arithmetic.java
+++ src/main/java/org/example/Arithmetic.java
@@ -3,6 +3,10 @@ package org.example;
 public class Arithmetic {
 
     public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            System.out.println("Error:Negative numbers are not allowed");
+            return Integer.MIN_VALUE;
+        }
         return a + b;
     }
     public int add(String a, String b) {
--- src/test/java/ArithmeticTest.java
+++ src/test/java/ArithmeticTest.java
@@ -1,8 +1,7 @@
 import org.example.Arithmetic;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class ArithmeticTest {
 
@@ -12,7 +11,7 @@ private Arithmetic arithmetic = new Arithmetic();
     @Test
     public void testAddingTwoIntegersWithNegative() {
         int result = arithmetic.add(2, -3);
-        assertFalse( result ==-1);
+        assertTrue( result == Integer.MIN_VALUE);
     }
 
     @Test
